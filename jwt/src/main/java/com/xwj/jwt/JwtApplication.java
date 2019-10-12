9e20fa7b3c90470571a09ceefda4698be465bf6a
xuwenjie <xuwenjie@travelsky.com>
2019-10-12 17:32:41 +0800

    jwt update

---
+++ jwt/src/main/java/com/xwj/jwt/JwtApplication.java
@@ -0,0 +1,15 @@
+package com.xwj.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class JwtApplication {
+    public static void main( String[] args ) {
+       SpringApplication.run(JwtApplication.class, args);
+    }
+}
