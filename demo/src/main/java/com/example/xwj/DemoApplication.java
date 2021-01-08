25fb60dedf9f4ba9dca972e50860b2f8d771ea4a
xuwenjie <xuwenjie@travelsky.com>
2021-01-08 20:42:25 +0800

    add

---
+++ demo/src/main/java/com/example/xwj/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.xwj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class DemoApplication {
+    public static void main( String[] args ) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
---
+++ demo/src/test/java/com/example/xwj/DemoApplicationTest.java
@@ -0,0 +1,25 @@
+package com.example.xwj;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Unit test for simple DemoApplication.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
